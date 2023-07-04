4c60a67b7e21f24d3151f8feee7afa5f0b18794d
2311ajay <92317294+2311ajay@users.noreply.github.com>
2023-07-04 21:03:48 +0800

    Errors are now displayed as a toast

--- components/AutoSearchComponent.js
+++ components/AutoSearchComponent.js
@@ -1,5 +1,10 @@
-import React, { useState } from 'react'
-import { Autocomplete } from '@react-google-maps/api'
+import React, { useState } from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import toast, { Toaster } from 'react-hot-toast';
+
+const notify = () => {
+   toast.error('Oops! We can\'t find this place on the map. Try a different keyword.', {id: "123"})
+};
 
 export default function AutoSearchComponent({ setLocation }) {
    const [autoCom, setAutoCom] = useState()
@@ -9,7 +14,7 @@ export default function AutoSearchComponent({ setLocation }) {
    }
 
    const onPlaceChanged = () => {
-      const place = autoCom.getPlace();
+      const place = autoCom?.getPlace();
       const geometry = place?.geometry;
       if (geometry) {
          const lat = place.geometry.location.lat();
@@ -17,20 +22,24 @@ export default function AutoSearchComponent({ setLocation }) {
          setLocation({ lat: place.geometry.location.lat(), lng: place.geometry.location.lng() })
          console.log('Get place', lat, lon)
       } else {
-         console.log('Autocomplete is not loaded yet!')
+         console.log('Autocomplete is not loaded yet!');
+         notify();
       }
    }
    return (
-      < Autocomplete
-         onLoad={onLoad}
-         onPlaceChanged={onPlaceChanged}
-         options={{
-            types: ["establishment"],
-            // componentRestrictions: { country: "ca" },
-         }}
-      >
-         <input className="form-control me-2" type="search" placeholder="Search places" aria-label="Search" />
-      </Autocomplete >
+      <div>
+         < Autocomplete
+            onLoad={onLoad}
+            onPlaceChanged={onPlaceChanged}
+            options={{
+               types: ["establishment"],
+               // componentRestrictions: { country: "ca" },
+            }}
+         >
+            <input className="form-control me-2" type="search" placeholder="Search places" aria-label="Search" />
+         </Autocomplete >
+         <Toaster />
+      </div>
    )
 }
 
